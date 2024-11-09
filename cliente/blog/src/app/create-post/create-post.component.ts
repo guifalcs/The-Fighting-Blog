e49404af740febbd5825dd09d3e6a75dc5358354
Guilherme <guifalcao2017@gmail.com>
2024-11-09 14:41:30 -0300

    começo do ajuste de rotas

--- cliente/blog/src/app/create-post/create-post.component.ts
+++ cliente/blog/src/app/create-post/create-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../page elements/header/header.component';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.scss'
 })
--- cliente/blog/src/app/posts/posts.component.ts
+++ cliente/blog/src/app/posts/posts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../page elements/header/header.component';
 
 @Component({
   selector: 'app-posts',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss'
 })
