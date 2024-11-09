97497c3427e5d7d746e86624ff642ffdf2721b24
Guilherme <guifalcao2017@gmail.com>
2024-11-09 11:54:42 -0300

    criação da página de criação de posts

--- cliente/blog/src/app/app.routes.ts
+++ cliente/blog/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },  
-    { path: 'home', component: HomeComponent },         
+    { path: 'home', component: HomeComponent },
+    { path: 'createPost', component: CreatePostComponent },
     { path: 'posts', component: PostsComponent }          
 ];
