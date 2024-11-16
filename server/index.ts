c8e3538a958ab02c390976be3378873a741350ca
Guilherme <guifalcao2017@gmail.com>
2024-11-16 20:04:17 -0300

    criacao do arquivo de rotas e do controller da tabela usuarios

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,22 @@ const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT || 8081;
+const prisma = new PrismaClient();
 
-app.listen(port, () => {
+async function main(){
+    await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            password: '123456'
+        }
+    });
+    const users = await prisma.user.findMany();
+    console.log(users);
+}
+
+app.listen(port, async() => {
     console.log(`Server is running on port ${port}`);
+    main()
 });
 
---
+++ server/routes.ts
@@ -0,0 +1,5 @@
+const router = express.router();
+
+
+
+module.exports = router;
